18c4fbda89d0dd889cbcda10c66be28d0c9b458f
bruno.barbosa <bruno.barbosa@linkupconsultoria.com.br>
2018-02-21 13:27:10 -0300

    Ajuste npm start

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,7 +5,7 @@
  */
 
 const mongoose = require('mongoose');
-const { wrap: async } = require('co');
+//const { wrap: async } = require('co');
 const { respond } = require('../utils');
 const User = mongoose.model('User');
 
